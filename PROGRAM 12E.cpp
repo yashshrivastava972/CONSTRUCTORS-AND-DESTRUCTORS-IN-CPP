90aecaf47d56a530154a9a604b15f09c9f425855
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-08-29 12:27:13 +0530

    Update PROGRAM 12E.cpp

--- PROGRAM 12E.cpp
+++ PROGRAM 12E.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+int count = 0;
+   class destruct{
+       public:
+       destruct()
+       {
+           count++;
+           cout<<"NO. OF OBJECTS CREATED:"<<count<<endl;
+       }
+       ~destruct()
+       {
+           count--;
+           cout<<"NO. OF OBJECTS DESTROYED:"<<count<<endl;
+       }
+   };
+   
+   int main(){
+       destruct aa, bb, cc;
+       { destruct dd;
+       }
+       return 0;
+   }
 
+// OUTPUT:
+
+// NO. OF OBJECTS CREATED:1
+// NO. OF OBJECTS CREATED:2
+// NO. OF OBJECTS CREATED:3
+// NO. OF OBJECTS CREATED:4
+// NO. OF OBJECTS DESTROYED:3
+// NO. OF OBJECTS DESTROYED:2
+// NO. OF OBJECTS DESTROYED:1
+// NO. OF OBJECTS DESTROYED:0
